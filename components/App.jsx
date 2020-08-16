d5916d448ac1504475c71ebf4da66099c7eff798
Alex Cruz <alexc017@yahoo.com>
2020-08-16 15:16:59 -0700

    Added feature:display the time of city

--- components/App.jsx
+++ components/App.jsx
@@ -3,32 +3,20 @@ import InputData from "./InputData.jsx";
 import CityDate from "./CityDate.jsx";
 import Weather from "./Weather.jsx";
 import Header from "./Header.jsx";
+
 function App() {
-  // call the api
   const [currentCity, setCurrentCity] = useState("");
   const [feelsLike, setFeelsLike] = useState();
   const [currentWeather, setCurrrentWeather] = useState();
   const [currentIcon, setCurrentIcon] = useState();
   const [weatherCondition, setWeatherCondition] = useState();
-  const [localEpoch, setLocalEpoch] = useState();
+
   // get current date from the city
-  const [date, setDate] = useState()
+  const [date, setDate] = useState();
 
-
-  // const getWeather = async (cityInput) => {
-  //   setCurrentCity(cityInput);
-  //   const response = await fetch(
-  //     `http://api.weatherapi.com/v1/current.json?key=0ba02e1aa5bf4ccd8de35307203107&q=${cityInput}`
-  //   );
-  //   const data = await response.json();
-  //   setAPIdata(data);
-
-  // };
-  // getWeather().then(() =>
-  // {
-  //   console.log(apiData.current);
-  // })
+  //function to fetch the api
   const getWeather = (cityInput) => {
+    //set the current city from the input when the button is cliked
     setCurrentCity(cityInput);
     return fetch(
       `http://api.weatherapi.com/v1/current.json?key=0ba02e1aa5bf4ccd8de35307203107&q=${cityInput}`
@@ -36,14 +24,23 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         // set the data
-       console.log(data)
-        setLocalEpoch(data.location.localtime_epoch);
+        console.log(data);
         setCurrentIcon(data.current.condition.icon);
         setWeatherCondition(data.current.condition.text);
         setFeelsLike(data.current.feelslike_f);
         setCurrrentWeather(data.current.temp_f);
+        // set the date by using the localEpoch
+        let time = new Date(data.location.localtime);
+        setDate(time.toLocaleString());
       })
-      .catch((err)=>alert("please input a correct city name"));
+      .catch((err) => {
+        alert("please input a correct city name");
+        setCurrentCity("");
+        setCurrentIcon("");
+        setWeatherCondition("");
+        setFeelsLike("");
+        setCurrrentWeather("");
+      });
   };
 
   return (
